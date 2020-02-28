6f1c7c210cec576e213e4b6f4888f45459437d58
blinkbat <guanhelengluo@163.com>
2020-02-28 22:51:05 +0800

    test

---
+++ src/test/java/com/seckill/dao/SeckillDaoTest.java
@@ -0,0 +1,33 @@
+package com.seckill.dao;
+
+import com.seckill.entity.Seckill;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/spring-dao.xml"})
+public class SeckillDaoTest {
+
+    @Resource
+    private SeckillDao seckillDao;
+
+    @Test
+    public void reduceNumber() throws Exception{
+
+    }
+
+    @Test
+    public void queryById() throws Exception {
+        long id = 1000;
+        Seckill seckill = seckillDao.queryById(id);
+        System.out.println(seckill.getName());
+    }
+
+    @Test
+    public void queryAll() {
+    }
+}
