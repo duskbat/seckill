15c9da230f2eb71f4c8fba754e01f42b9ee4d0c6
blinkbat <guanhelengluo@163.com>
2020-03-27 02:05:13 +0800

    存储过程

--- src/main/sql/seckill.sql
+++ src/main/sql/seckill.sql
@@ -4,7 +4,9 @@ DELIMITER $$ -- console ; 转换为 $ $
 -- 参数：in 输入参数; out 输出参数
 -- row_count():返回上一条修改类型sql(delete,insert,update)的影响行数
 -- row_count: 0:未修改数据; >0:表示修改的行数; <0:sql错误/未执行修改sql
-CREATE PROCEDURE `seckill`.`execute_seckill`(IN v_seckill_id bigint, IN v_phone BIGINT,
+
+-- 定义存储过程
+CREATE PROCEDURE `seckill`.`execute_seckill`(IN v_seckill_id BIGINT, IN v_phone BIGINT,
                                              IN v_kill_time TIMESTAMP, OUT r_result INT)
 BEGIN
     DECLARE insert_count INT DEFAULT 0;
@@ -14,12 +16,13 @@ BEGIN
     VALUES (v_seckill_id, v_phone, v_kill_time);
 
     SELECT ROW_COUNT() INTO insert_count;
+
     IF (insert_count = 0) THEN
         ROLLBACK;
-        SET r_result = -1;
+        SET r_result = -1; -- 重复秒杀
     ELSEIF (insert_count < 0) THEN
         ROLLBACK;
-        SET r_result = -2;
+        SET r_result = -2; -- 秒杀出错
     ELSE
         UPDATE seckill
         SET number = number - 1
@@ -27,6 +30,7 @@ BEGIN
           AND end_time > v_kill_time
           AND start_time < v_kill_time
           AND number > 0;
+
         SELECT ROW_COUNT() INTO insert_count;
         IF (insert_count = 0) THEN
             ROLLBACK;
@@ -41,7 +45,7 @@ BEGIN
     END IF;
 END;
 $$
--- 代表存储过程定义结束
+-- 存储过程定义结束
 
 DELIMITER ;
 
